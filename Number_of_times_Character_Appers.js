68ed25a585432ee8cd2bdad796b6be3a20553403
Meghana G H <meghana.gh2014@gmail.com>
2024-10-22 20:06:26 +0530

    Add files via upload

---
+++ Number_of_times_Character_Appers.js
@@ -0,0 +1,10 @@
+const str="bangalore"
+let count=0
+for(let i=0;i<str.length;i++){
+    if(str[i]=='a'){
+        count++
+    }
+}
+console.log(count)
+console.log(`The character a appers ${count}  times`)
+
