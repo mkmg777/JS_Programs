455ed3c917eb764317ebe2a54a56fe1ec4c0ccc0
Meghana G H <meghana.gh2014@gmail.com>
2024-10-23 20:30:51 +0530

    Add files via upload

---
+++ BooleanToString.js
@@ -0,0 +1,12 @@
+//create a function that takes boolean variable flag and returns it as a string
+
+function boolToString(str){
+    console.log(str.toString())
+}
+
+boolToString(true)
+boolToString(false)
+
+let bool = true;
+let text = bool.toString();
+console.log(text)
