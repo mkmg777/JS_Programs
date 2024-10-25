25d26628961d2c2bbdc63c026e4e670188e5978e
Meghana G H <meghana.gh2014@gmail.com>
2024-10-25 22:40:41 +0530

    Add files via upload

--- StringOperations-3.js
+++ StringOperations-3.js
@@ -53,3 +53,42 @@ function modifyLast(str,num){
 }
 console.log(modifyLast("Hello",3))
 console.log(modifyLast("hey",6))
+
+// How many D's are There?
+function countDs(str){
+    let count=0
+    for(let i=0;i<str.length;i++){
+        if((str[i]=='d') || (str[i]=='D')){
+            count++
+        }
+       
+    }
+    return count
+}
+console.log(countDs("My friend Dylan got distracted in school"))
+console.log(countDs("The rodents hibernated in their den"))
+
+// LowerCse,UpperCase or Mixed
+function getCase(str){
+    if(str==str.toLowerCase()){
+        return "lowerCase"
+    }else if( str==str.toUpperCase()){
+        return "uppercase"
+    }else{
+        return "mixedcase"
+    }
+}
+console.log(getCase("whisper..."))
+console.log(getCase("SHOUT!"))
+console.log(getCase("Indoor Voice"))
+
+// Scottish Screaming
+function toScottishScreaming(str){
+    str=str.replace(/[aeiouAEIOU]/gi,'e')
+    return str.toUpperCase()
+    
+
+}
+console.log(toScottishScreaming("hello world"))
+console.log(toScottishScreaming('Mr. Fox was very naughty'))
+console.log(toScottishScreaming('Butterflies are beautiful!'))
